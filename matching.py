b8e760cfb7fea146c66433819d974c289ce448cc
jfr573 <jfr573@cs.bham.ac.uk>
2017-02-19 19:11:03 +0000

    x

--- matching.py
+++ matching.py
@@ -1,7 +1,7 @@
-import xlrd
+import xlrd, json, openpyxl
 from os.path import join, isfile
 from os import listdir
-import json
+from pprint import pprint as pp
 
 
 document_home = "/home/joe/sajid/"
@@ -19,14 +19,12 @@ BORROWER = ("Borrower", 0)
 DATE = ("Announcement Date", 15)
 DESCRS = ("Role Descriptions", 44)
 MANAGERS = ("All Managers", 45)
-
-
-class Loan(object):
-    def __init__(self, date, borrower, role_descrs, managers):
-        self.date = date
-        self.borrower = borrower
-        self.role_descrs = role_desrcs
-        self.managers = managers
+LEAD_ROLES = {"CO-MANAGER", "LEAD MANAGER", "CO-LEAD MANAGER"}
+LEAD = "LEAD"
+PART = "PART"
+ID = "ID"
+N_PARTS = "N Parts"
+N_LEADS = "N Leads"
 
 
 def get_sheet(fname):
@@ -38,17 +36,23 @@ def get_sheet_data(sheet):
     for row in range(3, sheet.nrows):
         i = row - 3
         data.append({})
+        data[i][ID] = i
         data[i][BORROWER[0]] = sheet.cell_value(rowx=row, colx=BORROWER[1])
         data[i][DATE[0]] = sheet.cell_value(rowx=row, colx=DATE[1])
-        data[i]["Managers"] = list(zip(sheet.cell_value(rowx=row, colx=MANAGERS[1]).split("\n"), sheet.cell_value(rowx=row, colx=DESCRS[1]).split("\n")))
+        data[i][MANAGERS[0]] = list(zip(sheet.cell_value(rowx=row, colx=MANAGERS[1]).split("\n"), map(lambda x: LEAD if x in LEAD_ROLES else PART, sheet.cell_value(rowx=row, colx=DESCRS[1]).split("\n")), sheet.cell_value(rowx=row, colx=DESCRS[1]).split("\n")))
+        data[i][N_PARTS] = sum(p[1] == PART for p in data[i][MANAGERS[0]])
+        data[i][N_LEADS] = sum(p[1] == LEAD for p in data[i][MANAGERS[0]])
+        
     return data
 
 
-def all_loans_data():
+def all_loans_data(n = None):
+    if n is None:
+        n = len(loans_sheets_paths)
     data = []
-    for i in range(len(loans_sheets_paths)):
+    for i in range(n):
         data += get_sheet_data(get_sheet(loans_sheets_paths[i]))
-        print("File " + str(i) + " of " + str(len(loans_sheets_paths)) + " loaded")
+        print("File " + str(i + 1) + " of " + str(len(loans_sheets_paths)) + " loaded")
         
     return data
 
@@ -56,6 +60,40 @@ def all_loans_data():
 def all_role_descriptions(loans_data):
     descrs = set()
     for d in loans_data:
-        for m in d["Managers"]:
+        for m in d[MANAGERS[0]]:
             descrs.add(m[1])
     return descrs
+
+
+def make_sheet(loans):
+    wb = openpyxl.Workbook()
+    wb.name = "Loans data"
+    sheet = wb.active
+    sheet.title = "Loans data"
+
+    max_leads = max(l[N_LEADS] for l in loans)
+    max_parts = max(l[N_PARTS] for l in loans)
+
+    sheet.cell(row = 1, column = 1).value = ID
+    sheet.cell(row = 1, column = 2).value = "Date"
+
+    for i in range(max_leads):
+        sheet.cell(row = 1, column = i + 3).value  = "Lead " + str(i + 1)
+
+    for i in range(max_parts):
+        sheet.cell(row = 1, column = i + 3 + max_leads).value =  "Part " + str(i + 1)
+
+    for y, loan in enumerate(loans):
+        if (y % 10000 == 0):
+            print(str(y) + " loans written of " + str(len(loans)))
+        sheet.cell(row = y + 2, column = 1).value = loan[ID]
+        sheet.cell(row = y + 2, column = 2).value = loan[DATE[0]]
+
+        for i, lead in enumerate(filter(lambda x: x[1] == LEAD, loan[MANAGERS[0]])):
+            sheet.cell(row = y + 2, column = i + 3).value = lead[0]
+
+        for i, part in enumerate(filter(lambda x: x[1] == PART, loan[MANAGERS[0]])):
+            sheet.cell(row = y + 2, column = i + 3 + max_leads).value = part[0]
+            
+        
+    wb.save("Loans.xlsx")

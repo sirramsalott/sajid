773df01105bb5154eb50d6de83d018145a6250aa
jfr573 <jfr573@cs.bham.ac.uk>
2017-04-03 13:30:41 +0100

    everything

--- matching.py
+++ matching.py
@@ -6,11 +6,19 @@ from pprint import pprint as pp
 from functools import reduce
 from nltk import edit_distance
 from numpy import array
+from collections import Counter
+from company_name_similarity import CompanyNameSimilarity
+
 
 document_home = "/home/joe/sajid/"
 acquisitions_path = join(document_home, "M&As Data/Zephyr_Export_3.xls")
 loans_path = join(document_home, "Syndicated Loan Data/")
 loans_sheets_paths = []
+cm = CompanyNameSimilarity()
+
+
+with open("wc.txt") as f:
+    places = f.read().split("\n")
 
 
 for subdir in ["GlobalminusUSUK", "UK", "US"]:
@@ -37,7 +45,9 @@ TARGET_NAME = ("TARGET NAME", 4)
 ACQUISITION_DATE = ("COMPLETED DATE", 13)
 DEAL_STATUS = ("DEAL STATUS", 7)
 MATCHES = "MATCHES"
-THRESH = 0.4
+THRESH = 0.01
+STOPWORDS = ["bank", "sa", "ltd", "inc", "plc", "of", "the", "ag", "oao", "group", "de", "do", "di", "and", "banking", "banca"]
+THRESH = 0.2
 
 
 class Loan(object):
@@ -76,7 +86,8 @@ def get_sheet(fname):
 
 
 def normalise(name):
-    return name.lower().translate(str.maketrans("", "", punctuation)).split(" ")
+    #return [x for x in name.lower().translate(str.maketrans("", "", punctuation)).split(" ")]
+    return [x for x in name.lower().translate(str.maketrans("", "", punctuation)).split(" ") if x not in STOPWORDS]
 
 
 def get_acquisitions_data():
@@ -85,8 +96,8 @@ def get_acquisitions_data():
 
     for row in range(1, sheet.nrows):
         data.append(Acquisition(sheet.cell_value(rowx=row, colx=1),
-                                normalise(sheet.cell_value(rowx=row, colx=ACQUIROR_NAME[1])),
-                                normalise(sheet.cell_value(rowx=row, colx=TARGET_NAME[1])),
+                                sheet.cell_value(rowx=row, colx=ACQUIROR_NAME[1]),
+                                sheet.cell_value(rowx=row, colx=TARGET_NAME[1]),
                                 sheet.cell_value(rowx=row, colx=ACQUISITION_DATE[1]),
                                 sheet.cell_value(rowx=row, colx=DEAL_STATUS[1]),
                                 set(normalise(sheet.cell_value(rowx=row, colx=ACQUIROR_NAME[1]))),
@@ -99,6 +110,55 @@ def jaccard(a, b):
     return len(a.intersection(b)) / len(a.union(b))
 
 
+def a_stopwords(n):
+    acs = get_acquisitions_data()
+    c = Counter()
+    for a in acs:
+        for word in a.target_set:
+            c[word] += 1
+        for word in a.acquiror_set:
+            c[word] += 1
+    return [x[0] for x in c.most_common(n)]
+
+
+def similarity_score(s1, s2):
+    dif = s1.symmetric_difference(s2)
+    if len(dif) > 0 and all({x in places for x in dif}):
+        return 0
+    return cm.match_score(" ".join(s1), " ".join(s2))
+
+
+def compare_acquisitions():
+    wb = openpyxl.Workbook()
+    wb.name = "Name comparison"
+    acs = get_acquisitions_data()[1000:]
+    sheet = wb.active
+    sheet.title = "Name comparison"
+    i = 0
+    cm = CompanyNameSimilarity()
+    matches = set()
+    
+    for a in acs:
+        ja = " ".join(a.acquiror_set)
+        
+        for b in acs:
+            jb = " ".join(b.acquiror_set)    
+            match = (a.acquiror, b.acquiror)
+            j = similarity_score(a.acquiror_set, b.acquiror_set)
+
+            if 0.51 < j < 1 and match not in matches:
+                matches.add(match)
+                i += 1
+                sheet.cell(row = i, column = 1).value = a.acquiror
+                sheet.cell(row = i, column = 2).value = ja
+                sheet.cell(row = i, column = 3).value = b.acquiror
+                sheet.cell(row = i, column = 4).value = jb
+                sheet.cell(row = i, column = 5).value = j
+                sheet.cell(row = i, column = 6).value = " ".join(a.acquiror_set.symmetric_difference(b.acquiror_set))
+            
+    wb.save("Names.xlsx")
+
+
 def is_lead(manager):
     return manager[1] in LEAD_ROLES
 
@@ -107,9 +167,9 @@ def partition(xs, p):
     return ([x for x in xs if p(x)], [x for x in xs if not p(x)])
 
 
-def get_sheet_data(sheet, n, acquisitions):
+def get_sheet_data(sheet, n, acquisitions, lmax):
     data = []
-    for row in range(3, sheet.nrows):
+    for row in range(3, min(lmax + 3, sheet.nrows)):
         i = row - 3
         print(i)
         borrower = sheet.cell_value(rowx=row, colx=BORROWER[1])
@@ -120,13 +180,13 @@ def get_sheet_data(sheet, n, acquisitions):
         loan = Loan(n + i, date, [(normalise(l[0]), l[1]) for l in leads], [(normalise(p[0]), p[1]) for p in parts], borrower, [l[0] for l in leads], [p[0] for p in parts])
         
         ms = [normalise(m[0]) for m in managers]
-        for m1 in ms:
-            sm1 = set(m1)
-            for m2 in ms:
-                sm2 = set(m2)
-                if (m1 != m2):
-                    for a in acquisitions:
-                        if (jaccard(sm1, a.acquiror_set) < THRESH and jaccard(sm2, a.target_set) < THRESH) or (jaccard(sm2, a.acquiror_set) < THRESH and jaccard(sm1, a.target_set) < THRESH):
+        
+        for a in acquisitions:
+            for m1 in ms:
+                sm1 = set(m1)
+                for m2 in ms:
+                    sm2 = set(m2)
+                    if sm1 != sm2 and (jaccard(sm1, a.acquiror_set) < THRESH and jaccard(sm2, a.target_set) < THRESH) or (jaccard(sm2, a.acquiror_set) < THRESH and jaccard(sm1, a.target_set) < THRESH):
                             loan.matches.append((m1, m2))
         data.append(loan)
         
@@ -141,13 +201,16 @@ def get_sheet_data(sheet, n, acquisitions):
 def all_loans_data(n = None):
     a = get_acquisitions_data()
     if n is None:
-        n = len(loans_sheets_paths)
+        n = sys.maxsize
     data = []
     tot = 0
-    for i in range(n):
-        (temp, tot) = get_sheet_data(get_sheet(loans_sheets_paths[i]), tot, a)
+    i = 0
+    while i < len(loans_sheets_paths) and n > 0:
+        (temp, tot) = get_sheet_data(get_sheet(loans_sheets_paths[i]), tot, a, n)
         data += temp
+        n -= tot
         print("File " + str(i + 1) + " of " + str(len(loans_sheets_paths)) + " loaded")
+        i += 1
         
     return data
 
